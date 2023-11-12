87aea7542283e4bb1c46111a7fe59b1176f454a1
BethanyG <BethanyG@users.noreply.github.com>
2023-11-11 22:55:55 -0800

    Passes at alternate versions

---
+++ bottles.py
@@ -0,0 +1,77 @@
+class Bottles:
+    def song(self):
+        return self.verses(99, 0)
+
+    def verses(self, upper, lower):
+        return '\n'.join(self.verse(n) for n in range(upper, lower - 1, -1))
+
+    def verse(self, number):
+        bottle_number = BottleNumber.given(number)
+
+        return (
+            f'{str(bottle_number).capitalize()} of beer on the wall, ' \
+            f'{bottle_number} of beer.\n' \
+            f'{bottle_number.action()}, ' \
+            f'{bottle_number.successor()} of beer on the wall.\n'
+        )
+
+class BottleNumber:
+    @staticmethod
+    def given(number):
+        match number:
+            case 0:
+                cls = BottleNumber0
+            case 1:
+                cls = BottleNumber1
+            case 6:
+                cls = BottleNumber6
+            case _:
+                cls = BottleNumber
+
+        return cls(number)
+
+
+    def __init__(self, number):
+        self.number = number
+
+    def __repr__(self):
+        return self.quantity() + ' ' + self.container()
+
+    def quantity(self):
+        return str(self.number)
+
+    def container(self):
+        return 'bottles'
+
+    def action(self):
+        return f'Take {self.pronoun()} down and pass it around'
+
+    def pronoun(self):
+        return 'one'
+
+    def successor(self):
+        return BottleNumber.given(self.number - 1)
+
+class BottleNumber0(BottleNumber):
+    def quantity(self):
+        return 'no more'
+
+    def action(self):
+        return 'Go to the store and buy some more'
+
+    def successor(self):
+        return BottleNumber.given(99)
+
+class BottleNumber1(BottleNumber):
+    def container(self):
+        return 'bottle'
+
+    def pronoun(self):
+        return 'it'
+
+class BottleNumber6(BottleNumber):
+    def quantity(self):
+        return '1'
+
+    def container(self):
+        return 'six-pack'
